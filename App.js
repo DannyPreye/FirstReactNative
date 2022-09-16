921ef50ce3eaf315b62473fba10d9cfb87e2cd59
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-09-16 23:27:16 +0100

    First React Native App todo list

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
+import { useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View, TextInput, Image, Button, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 
 export default function App() {
+  const [goalText, setGoalText] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+
+  function goalInputHandler(enteredText) {
+    setGoalText(enteredText)
+
+
+  }
+  function deleteGoal(id) {
+    setCourseGoals(prev => {
+      return prev.filter((item) => item.id !== id)
+    })
+  }
+  function addGoalHandler() {
+    goalText && setCourseGoals(prev => [...prev, { text: goalText, id: Math.random().toString() }])
+    setGoalText("")
+    setModalIsVisible(false)
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer} >
+        <Button title='Add New Goal' color='#5e08cc' onPress={() => setModalIsVisible(true)} />
+        <GoalInput visible={modalIsVisible} onClose={() => setModalIsVisible(false)} goalInputHandler={goalInputHandler} addGoalHandler={addGoalHandler} value={goalText} />
+        <View style={styles.goalsContainer} >
+          {/* Flatlist is import for data you're getting from an api */}
+          <FlatList data={courseGoals} keyExtractor={(item, i) => {
+            return item.id
+          }}
+            alwaysBounceVertical={false} renderItem={itemData => {
+              return (
+                <GoalItem text={itemData.item.text} onPress={deleteGoal} id={itemData.item.id} />
+              )
+            }} />
+
+
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+
+
+  goalsContainer: {
+    flex: 5,
+
+  },
+
+
 });
